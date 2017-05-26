0d4692d8f18f68318d05a514a9fbc3f4fed313c1
Chris Henrick <chrishenrick@gmail.com>
2017-05-25 17:30:00 -0700

    static search box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // import components
 import NavBar from './NavBar';
+import SearchBox from './SearchBox';
 import LeafletMap from './LeafletMap';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        <SearchBox />
         <LeafletMap lat={36.897} lng={-74.619} zoom={5} />
       </div>
     );
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBox = () => (
+  <div className="SearchBox">
+    <input type="text" placeholder="Search a Starting Point" />
+  </div>
+);
+
+export default SearchBox;
