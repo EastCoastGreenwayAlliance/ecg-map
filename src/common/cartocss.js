4d22fa132fa5635c04b8862f50bb37c9a31ccddf
Chris Henrick <chrishenrick@gmail.com>
2017-05-30 17:50:57 -0700

    carocss styling of route based on line_type

--- src/common/cartocss.js
+++ src/common/cartocss.js
@@ -5,33 +5,59 @@ import { cartoTables } from './config';
 const { route_segments } = cartoTables;
 
 const cartocss = sls`
-  #${route_segments}{
+  /* color values */
+  @casing_light: #FFF;
+  @casing_dark: #333;
+  @route: #FF6600;
+  @trail: purple;
+  @transit: yellow;
 
+  #${route_segments}{
     /* casing so that route shows up over satellite imagery */
     ::outer {
-      line-color: #FFF;
+      line-color: @casing_light;
       line-width: 3.5;
+      line-join: round;
       line-opacity: 0.7;
 
-      [zoom > 16] {
+      [line_type = 'Transit or Ferry'] {
+        line-color: @casing_dark;
+        line-opacity: 0.6;
+      }
+
+      [zoom >= 14] {
         line-width: 5;
       }
 
-      [zoom > 18] {
+      [zoom >= 18] {
         line-width: 7;
       }
     }
 
     ::inner {
-      line-color: #FF6600;
+      line-color: @route;
       line-width: 2;
+      line-join: round;
       line-opacity: 0.7;
 
-      [zoom > 16] {
+      [line_type = 'Trail'] {
+        line-color: @trail;
+      }
+
+      [line_type = 'Trail, unpaved'] {
+        line-color: @trail;
+        line-dasharray: 4,4;
+      }
+
+      [line_type = 'Transit or Ferry'] {
+        line-color: @transit;
+      }
+
+      [zoom >= 14] {
         line-width: 3;
       }
 
-      [zoom > 18] {
+      [zoom >= 18] {
         line-width: 4;
       }
     }
