39a61c4a1b85ed63bb4037956c6c3c41c168e7e2
Chris Henrick <chrishenrick@gmail.com>
2017-06-26 11:20:03 -0700

    use tmp json for hydrating store.routing #35

--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,11 @@ import debounce from 'lodash/debounce';
 import rootReducer from './reducers/';
 import middleware from './middleware';
 
+// saved sample application state for store.routing, for testing / feature development,
+// so that we don't have to manually go through the UX steps to get routing data
+// be sure to remove this when app is ready for production!
+import routingState from './hydratedStateForTesting.json';
+
 function makeStore(initialState) {
   const store = createStore(
     rootReducer,
@@ -26,7 +31,8 @@ function makeStore(initialState) {
   return store;
 }
 
-const store = makeStore({});
+const store = makeStore({ routing: routingState });
+// const store = makeStore({});
 
 // fire redux-responsive on window resize event
 window.addEventListener('resize', debounce(() =>
