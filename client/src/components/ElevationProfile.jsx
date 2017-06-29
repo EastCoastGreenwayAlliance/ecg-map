f6da510b78467626a4b5b7a27aa8edfc51a7f54f
Chris Henrick <chrishenrick@gmail.com>
2017-06-29 13:17:58 -0700

    calc & display total distance & elev gain #35

--- client/src/components/ElevationProfile.jsx
+++ client/src/components/ElevationProfile.jsx
@@ -115,13 +115,42 @@ class ElevationProfile extends Component {
     const { elevData, route } = props;
     const { response } = route;
     const { downsampled } = response;
+    const METERS_TO_FEET = 3.28084;
+    const METERS_TO_MILES = 0.000621371;
+    const parsed = {};
 
-    // 400 data points is a little much for a 300px x 250px chart, filter?
-    // e.g., .filter((d, i) => i % 2 === 0)
-    return elevData.map((point, idx) => ({
-      y: Math.round(point.elevation * 3.28084), // elevation in feet
+    // format data for use with d3 area chart
+    parsed.elev = elevData.map((point, idx) => ({
+      y: Math.round(point.elevation * METERS_TO_FEET), // elevation in feet
       x: downsampled[idx].properties.miles,
     }));
+
+    // total distance of the route in miles
+    parsed.totalDistance = (() => {
+      if (route && route.response && route.response.properties
+        && route.response.properties.total_meters) {
+        // convert meters to miles
+        return Math.round(route.response.properties.total_meters * METERS_TO_MILES);
+      }
+      return null;
+    })();
+
+    // calculates the accumulated uphill gain over the route
+    parsed.elevGain = (() => {
+      const gain = parsed.elev.reduce((acc, cur, idx, arr) => {
+        if (arr[idx - 1] && cur.y > arr[idx - 1].y) {
+          acc += cur.y;
+        }
+        return acc;
+      }, 0);
+
+      const gainFeet = Math.round(gain * METERS_TO_FEET);
+
+      // so that we have thousands separated by commas
+      return gainFeet.toLocaleString();
+    })();
+
+    return parsed;
   }
 
   destroyChart() {
@@ -130,25 +159,32 @@ class ElevationProfile extends Component {
 
   renderAreaChart() {
     const { elevDataParsed } = this.state;
+
+    // margins to position the inner "g" group element
     const chartMargins = {
-      top: 20,
+      top: 40,
       right: 20,
       bottom: 45,
       left: 50
     };
+
+    // actual height & width of the SVG element
     const chartWidth = this.chartContainer.offsetWidth;
     const chartHeight = this.chartContainer.offsetHeight;
+
+    // dimensions of the area graph / inner "g" element
     const width = chartWidth - chartMargins.right - chartMargins.left;
     const height = chartHeight - chartMargins.top - chartMargins.bottom;
 
-    const minXValue = min(elevDataParsed, d => d.x);
-    const maxXValue = max(elevDataParsed, d => d.x);
+    // min & max values for calculating scales
+    const minXValue = min(elevDataParsed.elev, d => d.x);
+    const maxXValue = max(elevDataParsed.elev, d => d.x);
     const minYValue = 0;
-    const maxYValue = max(elevDataParsed, d => d.y);
+    const maxYValue = max(elevDataParsed.elev, d => d.y);
 
     // values for use with x-axis scale ticks
     const quantilesX = range(0, 1.25, 0.25).map(value =>
-      quantile(elevDataParsed, value, d => d.x));
+      quantile(elevDataParsed.elev, value, d => d.x));
 
     // values for use with y-axis scale ticks
     const quantilesY = [
@@ -159,33 +195,40 @@ class ElevationProfile extends Component {
       maxYValue,
     ];
 
+    // create the SVG element
     const svg = select(this.chartContainer).append('svg')
       .attr('height', chartHeight)
       .attr('width', chartWidth)
       .attr('id', 'elev-profile');
 
+    // append the parent group element
     const g = svg.append('g')
       .attr('transform', `translate(${chartMargins.left}, ${chartMargins.top})`);
 
+    // set x scale range and domain
     const xScale = scaleLinear()
       .range([0, width])
       .domain([minXValue, maxXValue]);
 
+    // set y scale range and domain
     const yScale = scaleLinear()
       .range([height, 0])
       .domain([minYValue, maxYValue]);
 
+    // set up the area path function
     const areaFn = area()
       .curve(curveBasis)
       .x(d => xScale(d.x))
       .y0(yScale(minYValue))
       .y1(d => yScale(d.y));
 
+    // create and append the path
     g.append('path')
-      .datum(elevDataParsed)
-      .attr('fill', 'steelblue')
+      .datum(elevDataParsed.elev)
+      .attr('fill', '#1482C5')
       .attr('d', areaFn);
 
+    // create the x axis and label
     g.append('g')
         .attr('transform', `translate(0, ${height})`)
         .call(axisBottom(xScale).tickValues(quantilesX))
@@ -195,15 +238,37 @@ class ElevationProfile extends Component {
         .attr('x', width / 2)
         .text('Distance in miles');
 
+    // create the y axis and label
     g.append('g')
         .call(axisLeft(yScale).tickValues(quantilesY))
       .append('text')
-        .attr('fill', '#333')
         .attr('transform', 'rotate(90)')
         .attr('y', 40)
-        .attr('x', height - 10)
+        .attr('x', height - 30)
         .attr('text-anchor', 'end')
         .text('Height in feet');
+
+    // text for total distance
+    g.append('g')
+      .append('text')
+        .classed('heading', true)
+        .attr('y', -20)
+        .attr('x', -40)
+        .text('Total dist:')
+      .append('tspan')
+        .text(` ${elevDataParsed.totalDistance} mi`);
+
+    // text for total elevation gain
+    g.append('g')
+      .append('text')
+        .classed('heading', true)
+        .attr('y', -20)
+        .attr('x', 230)
+        .attr('text-anchor', 'end')
+        .attr('startOffset', '100%')
+        .text('Elev gain:')
+      .append('tspan')
+        .text(` ${elevDataParsed.elevGain} ft`);
   }
 
   render() {
