c882f4e75fda9da3696bbc8bb0494e20f356f061
Chris Henrick <chrishenrick@gmail.com>
2017-07-05 16:37:35 -0700

    formatting & semicolons for lines prod table update sql #46

--- sql/ecg_lines_update_prod_table.sql
+++ sql/ecg_lines_update_prod_table.sql
@@ -4,28 +4,46 @@
 
 -- Update existing records
 UPDATE ecg_route_lines_prod b
-SET the_geom = a.the_geom,
-ecg_review = a.ecg_review,
-direction = a.direction,
-title = a.title,
-line_type = a.line_type,
-meters = a.meters,
-datetime_modified = current_timestamp
+SET
+  the_geom = a.the_geom,
+  ecg_review = a.ecg_review,
+  direction = a.direction,
+  title = a.title,
+  line_type = a.line_type,
+  meters = a.meters,
+  datetime_modified = current_timestamp
 FROM ecg_route_lines a
-WHERE a.pline_id = b.pline_id
+WHERE a.pline_id = b.pline_id;
 
 
 -- Delete old records that no longer exist
 DELETE FROM ecg_route_lines_prod a
 WHERE a.pline_id NOT IN (
   SELECT distinct pline_id FROM ecg_route_lines
-  )
+);
 
 
 -- Insert new rows
-INSERT INTO ecg_route_lines_prod (the_geom, pline_id, ecg_review, direction, title, line_type, meters, datetime_modified)
-SELECT the_geom, pline_id, ecg_review, direction, title, line_type, meters, current_timestamp
+INSERT INTO ecg_route_lines_prod (
+  the_geom,
+  pline_id,
+  ecg_review,
+  direction,
+  title,
+  line_type,
+  meters,
+  datetime_modified
+)
+SELECT
+  the_geom,
+  pline_id,
+  ecg_review,
+  direction,
+  title,
+  line_type,
+  meters,
+  current_timestamp
 FROM ecg_route_lines a
 WHERE a.pline_id NOT IN (
   SELECT distinct pline_id FROM ecg_route_lines_prod
-  )
+);
