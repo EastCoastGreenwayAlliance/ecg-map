8f400ea8323e7769c99f45413f72db3840f2d811
Chris Henrick <chrishenrick@gmail.com>
2017-07-06 15:18:41 -0700

    set active turning readout message using onpath #37
    When the feature is first activated, displays finding your location,
    when the user is away from the route it tells the user to return to the route,
    when the user is on the route, tells them the next turn.

--- client/src/components/ActiveTurningReadout.jsx
+++ client/src/components/ActiveTurningReadout.jsx
@@ -1,42 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ActiveTurningReadout = (props) => {
-  const { enabled, disableActiveTurning, distance, onpath, transition_code,
-    transition_text, isMobile } = props;
+class ActiveTurningReadout extends Component {
+  static propTypes = {
+    enabled: PropTypes.bool.isRequired,
+    currentplace: PropTypes.string,
+    disableActiveTurning: PropTypes.func.isRequired,
+    distance: PropTypes.string,
+    transition_text: PropTypes.string,
+    transition_code: PropTypes.string,
+    onpath: PropTypes.bool.isRequired,
+    isMobile: PropTypes.bool.isRequired,
+  };
 
-  // only want to display this on mobile and if active turning is enabled
-  if (!isMobile || !enabled) return null;
+  constructor(props) {
+    super(props);
+    this.offRouteMessage = 'You don\'t appear to be on the highlighted portion of the East Coast Greenway route.';
+    this.state = {
+      cueMessage: props.onpath ? props.transition_text : this.offRouteMessage
+    };
+  }
 
-  return (
-    <div className="ActiveTurningReadout">
-      <div className="active-turning-cancel">
-        <button className="button-cancel" onClick={() => disableActiveTurning()}>Cancel</button>
+  componentWillReceiveProps(nextProps) {
+    const { currentplace, onpath, transition_text, enabled } = nextProps;
+
+    // when the user toggles the active turning feature, tell them it's working
+    if (enabled && !this.props.enabled) {
+      this.setState({
+        cueMessage: 'determining your location...'
+      });
+    }
+
+    // active turning has been enabled and someone is on path and we have transition text
+    if (enabled && this.props.enabled && onpath && transition_text) {
+      this.setState({
+        cueMessage: transition_text
+      });
+    }
+
+    // active turning has been enabled and someone is off path
+    if (enabled && this.props.enabled && !onpath && currentplace) {
+      this.setState({
+        cueMessage: currentplace
+      });
+    }
+  }
+
+  render() {
+    const { cueMessage } = this.state;
+    const { enabled, disableActiveTurning, distance, onpath, transition_code,
+      isMobile } = this.props;
+
+    // only want to display this on mobile and if active turning is enabled
+    if (!isMobile || !enabled) return null;
+
+    return (
+      <div className="ActiveTurningReadout">
+        <div className="active-turning-cancel">
+          <button className="button-cancel" onClick={() => disableActiveTurning()}>Cancel</button>
+        </div>
+        <div className="active-turning-cue">
+          <span className={`active-turning-icon turn-icon turn-icon-${transition_code}`} />
+          {
+            onpath ?
+              <p className="active-turning-on-path-cue"><span>{ distance }</span>{` ${cueMessage}`}</p> :
+              <p className="active-turning-off-path-cue">{ cueMessage }</p>
+          }
+        </div>
       </div>
-      <div className="active-turning-cue">
-        <span className={`active-turning-icon turn-icon turn-icon-${transition_code}`} />
-        {
-          onpath ?
-            <p className="active-turning-on-path-cue"><span>{ distance }</span>{` ${transition_text}`}</p> :
-            <p className="active-turning-off-path-cue">
-              You don't appear to be on the highlighted portion of the
-              East Coast Greenway route.
-            </p>
-        }
-      </div>
-    </div>
-  );
-};
-
-ActiveTurningReadout.propTypes = {
-  enabled: PropTypes.bool.isRequired,
-  currentplace: PropTypes.string,
-  disableActiveTurning: PropTypes.func.isRequired,
-  distance: PropTypes.string,
-  transition_text: PropTypes.string,
-  transition_code: PropTypes.string,
-  onpath: PropTypes.bool.isRequired,
-  isMobile: PropTypes.bool.isRequired,
-};
+    );
+  }
+}
 
 export default ActiveTurningReadout;
