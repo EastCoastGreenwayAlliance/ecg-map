c93ca328b8cf7842495ceb1645a1239821fe102f
Chris Henrick <chrishenrick@gmail.com>
2017-07-06 10:48:10 -0700

    only show active turning ui if enabled & mobile device #37

--- client/src/components/ActiveTurningReadout.jsx
+++ client/src/components/ActiveTurningReadout.jsx
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ActiveTurningReadout extends Component {
-  static propTypes = {
-    currentplace: PropTypes.string,
-    distance: PropTypes.string,
-    transition_text: PropTypes.string,
-    transition_code: PropTypes.string,
-    onpath: PropTypes.bool,
-  }
+const ActiveTurningReadout = (props) => {
+  const { enabled, onpath, currentplace, distance, transition_code, transition_text,
+    isMobile } = props;
 
-  render() {
-    const { onpath, currentplace, distance, transition_code, transition_text } = this.props;
+  // only want to display this on mobile and if active turning is enabled
+  if (!isMobile || !enabled) return null;
 
-    return (
-      <div className="ActiveTurningReadout">
-        <p>
+  return (
+    <div className="ActiveTurningReadout">
+      <p>
+        Currently: { currentplace }
+      </p>
+      <p>
+        { distance } { transition_text }
+      </p>
+      <p>
+        Icon: { transition_code }
+      </p>
+      <p>
+        Is On Path? { onpath ? 'yes' : 'no' }
+      </p>
+    </div>
+  );
+};
 
-          Currently: { currentplace }
-        </p>
-        <p>
-          { distance } { transition_text }
-        </p>
-        <p>
-          Icon: { transition_code }
-        </p>
-        <p>
-          Is On Path? { onpath }
-        </p>
-      </div>
-    );
-  }
-}
+ActiveTurningReadout.propTypes = {
+  enabled: PropTypes.bool.isRequired,
+  currentplace: PropTypes.string,
+  distance: PropTypes.string,
+  transition_text: PropTypes.string,
+  transition_code: PropTypes.string,
+  onpath: PropTypes.bool.isRequired,
+  isMobile: PropTypes.bool.isRequired,
+};
 
 export default ActiveTurningReadout;
--- client/src/containers/ActiveTurningReadoutConnected.jsx
+++ client/src/containers/ActiveTurningReadoutConnected.jsx
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import ActiveTurningReadout from '../components/ActiveTurningReadout';
 
 // placeholder for when we need to massage the structure instead of keeping it as-is
-const mapStateToProps = ({ activeturning }) => ({ ...activeturning });
+const mapStateToProps = ({ activeturning, browser }) => ({
+  ...activeturning,
+  isMobile: !browser.greaterThan.small
+});
 
 export default connect(mapStateToProps, null)(ActiveTurningReadout);
