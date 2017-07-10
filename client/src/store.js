d60cb240e76689a70ff2054ccfcea3950bd6b849
Chris Henrick <chrishenrick@gmail.com>
2017-07-10 16:02:19 -0700

    set store.routing from url query params #38

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,11 @@ import debounce from 'lodash/debounce';
 
 import rootReducer from './reducers/';
 import middleware from './middleware';
+import { parseURLQueryParams, preloadRoutingState } from './common/api';
 
 // saved sample application state for store.routing, for testing / feature development,
 // so that we don't have to manually go through the UX steps to get routing data
-import routingState from './hydratedStateForTesting.json';
+// import routingState from './hydratedStateForTesting.json';
 
 // enable redux dev tools: https://github.com/zalmoxisus/redux-devtools-extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -33,7 +34,16 @@ function makeStore(initialState) {
   return store;
 }
 
-const preloadedState = process.env.NODE_ENV === 'production' ? {} : { routing: routingState };
+// temp state for testing app after a route has been successfully searched & loaded
+// const preloadedState = process.env.NODE_ENV === 'production' ? {} : { routing: routingState };
+
+// look for start and end lat lons in the query params
+const startEnd = parseURLQueryParams();
+// create the preloaded state for store.routing if any query params exist
+const preloadedState = {
+  routing: preloadRoutingState(startEnd),
+};
+// set our default application state
 const store = makeStore(preloadedState);
 
 // fire redux-responsive on window resize event
