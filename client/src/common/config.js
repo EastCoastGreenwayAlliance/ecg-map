46ebde6a55cbc780fc8bffe01a7b5c9b5bf7fa2e
Greg Allensworth <gregor@greeninfo.org>
2019-06-17 14:04:37 -0700

    https://github.com/GreenInfo-Network/ecg-route-server/issues/2 Routing working

--- client/src/common/config.js
+++ client/src/common/config.js
@@ -43,3 +43,7 @@ export const maxGeoBounds = [[18.312811, -110.830078], [53.278353, -45.351563]];
 // some universal constants such as unit conversions
 export const METERS_TO_MILES = 1609;
 export const METERS_TO_FEET = 3.281;
+
+// the URL of the routing API server
+// export const ROUTER_API_URL = '/route/';
+export const ROUTER_API_URL = 'https://router.greenway.org/';
